701c7099bacd54fa247b0e05a978e781cefb1eac
Christian Roger <ChristianRogerpm@gmail.com>
2018-11-16 21:54:55 -0500

    ok

--- app/src/main/java/com/dev/christian/app/Pkg_Activity/IntroActivity.java
+++ app/src/main/java/com/dev/christian/app/Pkg_Activity/IntroActivity.java
@@ -10,6 +10,7 @@ public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //New Test Commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         new Thread(new Runnable() {
